842f36ef2b33617bd7a550a046f4dcce172ba996
Danu <danu@prakashs-MacBook-Air.local>
2025-01-06 12:02:26 +0530

    new

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const port = 3010;
 
+app.use(express.json());
+
 app.use(express.static('static'));
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log('Connected to database'))
+  .catch((err) => console.log('Error connecting to database:', err));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
